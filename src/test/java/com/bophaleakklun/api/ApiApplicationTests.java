f8c31a346245e16ee1c41f2f2ac63ad5b27857a6
Senkosal <senkosal@gmail.com>
2025-11-11 21:47:52 +0700

    init project

---
+++ src/test/java/com/bophaleakklun/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bophaleakklun.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
